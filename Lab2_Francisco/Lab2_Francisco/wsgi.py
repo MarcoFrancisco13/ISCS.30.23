baeef1682f94a0474427212b2f2b0e3dd9220368
Marco <marco.francisco@obf.ateneo.edu>
2021-03-18 17:48:48 +0800

    Added Hello World file

---
+++ Lab2_Francisco/Lab2_Francisco/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Lab2_Francisco project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lab2_Francisco.settings')
+
+application = get_wsgi_application()
